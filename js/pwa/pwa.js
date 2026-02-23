3aad1114e7b944b37c4578117f03a5375adc0759
WillowSage <lrdunn301@gmail.com>
2026-02-23 12:02:23 -0600

    adding update banner

--- js/pwa/pwa.js
+++ js/pwa/pwa.js
@@ -1,7 +1,24 @@
 import { registerSW } from "virtual:pwa-register";
+import { hideUpdateBanner, showUpdateBanner } from "./updateBanner.js";
 
 if (import.meta.env.PROD) {
-  registerSW({
-    immediate: true
+  const updateSW = registerSW({
+    immediate: true,
+    onNeedRefresh() {
+      console.log("[pwa] onNeedRefresh fired");
+      showUpdateBanner({
+        onRefresh: async () => {
+          console.log("[pwa] Refresh clicked");
+          await updateSW(true);
+        },
+        onDismiss: () => {
+          console.log("[pwa] Later clicked");
+          hideUpdateBanner();
+        }
+      });
+    },
+    onOfflineReady() {
+      console.log("[pwa] onOfflineReady fired");
+    }
   });
 }
